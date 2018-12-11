23e4eebb8053c5a6085e5985e89aeaccbc66aaf6
Mario Linguito <32237365+mariolinguito@users.noreply.github.com>
2018-12-11 20:37:10 +0100

    Add files via upload

---
+++ Daily Coding Problem #1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ver_sum_1(int [], int, int); 
+int ver_sum_2(int [], int, int);
+
+int main(int argc, char *argv[]) {
+	int A[4] = {10, 15, 3, 7};
+	int k1 = 17; 
+	int k2 = 11; 
+	int k3 = 13; 
+
+	printf("Version 1 @ %d\n", ver_sum_1(A, 4, k1));
+	printf("Version 2 @ %d\n", ver_sum_2(A, 4, k3));
+}
+
+int ver_sum_1(int A[], int size, int k) {
+	int result = 0; 
+
+	for(int i = 0; i < 4; i++) {
+		if(k-A[i] > 0) {
+			for(int j = 0; j < 4; j++) {
+				if((k-A[i]) == A[j]) {
+					result = 1;
+					break; 	
+				}
+			}
+		}
+	}
+
+	return result; 
+}
+
+int ver_sum_2(int A[], int size, int k) {
+	// It works only if the array is sorted. 
+	int result = 0; 
+	int i = 0; 
+	int j = size; 
+
+	while(i < j) {
+		if(A[i]+A[j] == k) {
+			result = 1; 
+		       	break; 	
+		} else if(A[i]+A[j] > k) {
+			j -= 1; 
+		} else {
+			i += 1; 
+		}
+	}
+
+	return result; 
+}
